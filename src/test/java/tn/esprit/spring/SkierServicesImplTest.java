90a606e99062107ff072fc6b65af20cd6fef4350
unknown <nassim.allouche@esprit.tn>
2023-11-12 15:04:49 +0100

    final commit

--- src/test/java/tn/esprit/spring/SkierServicesImplTest.java
+++ src/test/java/tn/esprit/spring/SkierServicesImplTest.java
@@ -1,77 +1,34 @@
 package tn.esprit.spring;
 
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+import sun.jvm.hotspot.utilities.Assert;
 import tn.esprit.spring.entities.Skier;
 import tn.esprit.spring.entities.Subscription;
-import tn.esprit.spring.entities.TypeSubscription;
-import tn.esprit.spring.repositories.*;
+import tn.esprit.spring.repositories.ISkierRepository;
+import tn.esprit.spring.services.ISkierServices;
 import tn.esprit.spring.services.SkierServicesImpl;
 
-import java.time.LocalDate;
 import java.util.List;
 
-@RunWith(MockitoJUnitRunner.class)
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 public class SkierServicesImplTest {
 
     @Mock
     private ISkierRepository skierRepository;
 
-    @Mock
-    private IPisteRepository pisteRepository;
-
-    @Mock
-    private ICourseRepository courseRepository;
-
-    @Mock
-    private IRegistrationRepository registrationRepository;
-
-    @Mock
-    private ISubscriptionRepository subscriptionRepository;
-
-    private SkierServicesImpl skierServicesImpl;
-
-    @BeforeEach
-    public void setUp() {
-        skierServicesImpl = new SkierServicesImpl(skierRepository, pisteRepository, courseRepository, registrationRepository, subscriptionRepository);
-    }
-
+    @InjectMocks
+    private SkierServicesImpl skierServicesImpl ;
     @Test
     public void retrieveAllSkiersTest() {
         List<Skier> skiers = skierServicesImpl.retrieveAllSkiers();
         Mockito.verify(skierRepository).findAll();
-        Assert.assertNotNull(skiers);
-    }
 
-    @Test
-    public void addSkierTest() {
-        Skier skier = new Skier();
-        Subscription subscription = new Subscription();
-        skier.setSubscription(subscription);
-
-        Skier savedSkier = skierServicesImpl.addSkier(skier);
-        Mockito.verify(skierRepository).save(skier);
-        Assert.assertEquals(skier, savedSkier);
-    }
-
-
-
-    @Test
-    public void removeSkierTest() {
-        Long numSkier = 3L;
-
-        Mockito.doNothing().when(skierRepository).deleteById(numSkier);
-
-        skierServicesImpl.removeSkier(numSkier);
-        Mockito.verify(skierRepository).deleteById(numSkier);
     }
 }
-
-
-
-
